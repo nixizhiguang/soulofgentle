061e103db1c888493305d5627d5821dc7b03dbb2
tengwl <tengwl@500wan.com>
2019-03-01 18:22:22 +0800

    * 一堆坑

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import AppNavigator from './navigation/AppNavigator';
-import { httpsOverHttp, httpOverHttp } from 'tunnel-agent';
+//import { httpsOverHttp, httpOverHttp } from 'tunnel-agent';
 import axios from 'axios';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import userReducer from 'reducers/user';
+import userReducer from './reducers/user';
 
 // 防止console系列函数影响性能，可以用插件在打包过程中直接去掉这些函数的调用
 if (!__DEV__) {
@@ -16,21 +16,23 @@ if (!__DEV__) {
     debug: () => {},
     error: () => {}
   };
+
+ /* // 代理 正式版删掉
+  const TUNNEL_OPTIONS = {
+    proxy: {
+      host:'127.0.0.1',
+      port: 1080
+    }
+  };
+
+  axios.interceptors.request.use(function (config) {
+    config.proxy = false; // 强制禁用环境变量中的代理配置
+    config.httpAgent = httpOverHttp(TUNNEL_OPTIONS);
+    config.httpsAgent = httpsOverHttp(TUNNEL_OPTIONS);
+    return config;
+  });*/
+
 }
-
-const TUNNEL_OPTIONS = {
-  proxy: {
-    host:'127.0.0.1',
-    port: 1080
-  }
-};
-
-axios.interceptors.request.use(function (config) {
-  config.proxy = false; // 强制禁用环境变量中的代理配置
-  config.httpAgent = httpOverHttp(TUNNEL_OPTIONS);
-  config.httpsAgent = httpsOverHttp(TUNNEL_OPTIONS);
-  return config;
-});
 
 class AppContainer extends React.Component {
   render() {
@@ -49,8 +51,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   }
 });
+// 网易mumu模拟器连接adb的方式
 //adb connect 127.0.0.1:7555
 
+// 连接redux
 const store = createStore(userReducer);
 
 export default class App extends React.Component {
